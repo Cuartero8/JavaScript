35b958a72070b2b1858d640fba1be385603616b5
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2023-12-10 23:17:13 +0100

    Ej Objet Copying

--- 1_ Review Curse Intro/5_ Objets Copying/exercise/exercise.js
+++ 1_ Review Curse Intro/5_ Objets Copying/exercise/exercise.js
@@ -7,6 +7,9 @@ const person1 = {
 const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
+person2.firstName = "Simon";
 
 console.log(person1);
 console.log(person2);
+
+/*  Obtenemos Simon en ambos porque person2 está asignado por referencia y por lo tanto no es una copia independiente del objeto person1  */
