d6398b3fad06834c631b377962a63e3f0c7a00a6
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2023-12-11 11:11:04 +0100

    Ej Optional Chaining

--- 1_ Review Curse Intro/10_ Optional Chaining/exercise.js
+++ 1_ Review Curse Intro/10_ Optional Chaining/exercise.js
@@ -1,5 +1,11 @@
-const order = {};
+const order = {
+  // customer: {
+  //   address: {
+  //     city: ""
+  //   },
+  // },
+};
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
+if (!order.customer?.address?.city) {
   console.log('City is required');
-}
+};
